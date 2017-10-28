23d6103b89618a4cb905be9dfea065b2482ff085
Jim Chestnutt <jim.chestnutt@live.co.uk>
2017-10-28 09:16:02 +0100

    header nav and core elements

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -2,7 +2,44 @@
 /*global require:true*/
 /*eslint no-undef: "error"*/
 
+require('./vendor/modernizr.js');
+
 const $ = require('jquery');
+
+// main menu toggle
+$('.js-toggle-nav.header__toggle').click(function() {
+	$(this).toggleClass('is-active');
+	$('.header__nav').toggleClass('is-active-nav');
+	$('body').toggleClass('mobile-menu--active');
+});
+
+// close menu on click outside or esc key
+function closeHeaderMenu() {
+	$('.js-toggle-nav.header__toggle').removeClass('is-active');
+	$('.header__nav').removeClass('is-active-nav');
+	$('body').removeClass('mobile-menu--active');
+}
+
+document.addEventListener('keyup', (event) => {
+	const keyName = event.key;
+
+	if (keyName === 'Escape') {
+		if($('body').hasClass('mobile-menu--active')) {
+			closeHeaderMenu();
+		}
+	}
+});
+
+function handleMenuClick(event) {
+	if($('body').hasClass('mobile-menu--active')) {
+		if( ((event.target).closest('.header__nav') == null) && ((event.target).closest('.header__toggle') == null) ) {
+			closeHeaderMenu();
+		}
+	}
+}
+
+document.addEventListener('click', (event) => { handleMenuClick(event); });
+document.addEventListener('touchend', (event) => { handleMenuClick(event); });
 
 //const $ = 'hi';
 
